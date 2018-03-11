db5e15bf05257cac6468978f3ee1a35e681d53e9
Garrett Hunter <garrett.hunter@blacktower.com>
2018-03-10 18:01:17 -0800

    merging 1.0.2 changes

--- includes/class-thematic-maps-activator.php
+++ includes/class-thematic-maps-activator.php
@@ -1,41 +1,3 @@
-<<<<<<< HEAD
-<?php
-
-/**
- * Fired during plugin activation
- *
- * @link       https://github.com/garretthunter
- * @since      1.0.0
- *
- * @package    Thematic_Maps
- * @subpackage Thematic_Maps/includes
- */
-
-/**
- * Fired during plugin activation.
- *
- * This class defines all code necessary to run during the plugin's activation.
- *
- * @since      1.0.0
- * @package    Thematic_Maps
- * @subpackage Thematic_Maps/includes
- * @author     Garrett Hunter <garrett.hunter@blacktower.com>
- */
-class Thematic_Maps_Activator {
-
-	/**
-	 * Short Description. (use period)
-	 *
-	 * Long Description.
-	 *
-	 * @since    1.0.0
-	 */
-	public static function activate() {
-
-	}
-
-}
-=======
 <?php
 
 /**
@@ -108,4 +70,3 @@ class Thematic_Maps_Activator {
 	}
 
 }
->>>>>>> iso regions added
