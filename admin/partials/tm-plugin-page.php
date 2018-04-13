2c4c67d08eac16f0ced65c8a36393ae34bdc3d3e
Garrett Hunter <garrett.hunter@blacktower.com>
2018-04-13 16:40:56 -0700

    interim changes

--- admin/partials/tm-plugin-page.php
+++ admin/partials/tm-plugin-page.php
@@ -11,34 +11,34 @@
 ?>
 
 <div class="wrap">
+
+    <div id="icon-themes" class="icon32"></div>
+    <h2>Thematic Maps</h2>
+	<?php settings_errors(); ?>
+
+	<?php
+	$active_tab = isset( $_GET[ 'tab' ] ) ? $_GET[ 'tab' ] : 'settings';
+	?>
+
+    <h2 class="nav-tab-wrapper">
+        <a href="?page=thematic_maps_plugin&tab=settings" class="nav-tab <?php echo $active_tab == 'settings' ? 'nav-tab-active' : ''; ?>">Settings</a>
+        <a href="?page=thematic_maps_plugin&tab=maps" class="nav-tab <?php echo $active_tab == 'maps' ? 'nav-tab-active' : ''; ?>">Maps</a>
+    </h2>
+
    <form method="post" action="options.php">
         <?php
         settings_errors();
-        settings_fields( $this->plugin_name.'_plugin' );
-        do_settings_sections( $this->plugin_name.'_plugin');
-        submit_button();
+
+        if ( 'settings' == $active_tab ) {
+	        settings_fields( $this->plugin_name.'_plugin' );
+	        do_settings_sections( $this->plugin_name.'_plugin');
+	        submit_button();
+        } else { ?>
+            <h2>Maps</h2>
+       <?php
+        }
+
         ?>
     </form>
-    <h2><?php echo esc_html( "Thematic Maps Settings" ); ?></h2>
-    <div id="poststuff">
-        <form method="post" action="<?php echo esc_html( admin_url( 'admin-post.php' ) ); ?>">
-            <table class="form-table">
-                <tbody>
-                <tr>
-                    <th scope="row">
-                        <label for="">Google API Key</label>
-                    </th>
-                    <td>
-                        <input type="text" name="maps_apikey" value="BOO" />
-                    </td>
-                </tr>
-                </tbody>
-            </table>
-			<?php
-			wp_nonce_field( 'acme-settings-save', 'acme-custom-message' );
-			submit_button();
-			?>
-        </form>
-    </div>
 
 </div><!-- /.wrap -->
